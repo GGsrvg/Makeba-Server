8b4951ce52722c3714ce951da7f4dde3f1a0afbb
GGsrvg <x@y.z>
2021-01-16 18:28:08 +0300

    add video type

--- internal/server/models/fileType.go
+++ internal/server/models/fileType.go
@@ -5,5 +5,6 @@ type FileType string
 const (
 	BOOK  FileType = "BOOK"  // pdf
 	TABLE FileType = "TABLE" // excel
-	IMAGE FileType = "IMAGE" // png, jpg, jpeg
+	IMAGE FileType = "IMAGE" // png, jpg, jpeg, gif
+	VIDEO FileType = "VIDEO" // mp4 or another
 )
